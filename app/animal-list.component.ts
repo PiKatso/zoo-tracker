20a0564ad5ac12b901bf7734dde646bc9181856c
Kat Gifford <katgiff2007@gmail.com>
2017-06-09 13:02:55 -0700

    add show details

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -5,14 +5,30 @@ import { Animal } from './animal.model';
   selector: 'animal-list',
   template: `
   <div *ngFor="let animal of childAnimalList">
-     <h4><a>{{animal.species}}</a></h4>
+     <h4><a (click)="showDeets(animal)">{{animal.species}}</a></h4>
   </div>
 
+  <div *ngIf="animalShowDeets" class="keg-details">
+      <h3>{{animalShowDeets.species}} : {{animalShowDeets.name}}</h3>
+      <p>{{animalShowDeets.name}} is {{animalShowDeets.age}}</p>
+      <p>{{animalShowDeets.name}}'s' diet is  {{animalShowDeets.diet}}</p>
+      <p>{{animalShowDeets.name}} is located:  {{animalShowDeets.price}}</p>
+      <p>{{animalShowDeets.name}} has {{animalShowDeets.caretakers}} caretakers</p>
+      <p>{{animalShowDeets.name}} is {{animalShowDeets.sex}}</p>
+      <p>{{animalShowDeets.name}} likes {{animalShowDeets.likes}}</p>
+      <p>{{animalShowDeets.name}} dislikes {{animalShowDeets.dislikes}}</p>
+    </div>
   `
 })
 
 export class AnimalListComponent {
   @Input() childAnimalList: Animal[];
+
+  animalShowDeets = null;
+
+  showDeets(animal) {
+    this.animalShowDeets = animal;
+  }
 
 
 }
